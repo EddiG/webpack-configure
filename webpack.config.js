33396f32b5282b0de1fa712492d0b4365b1925ce
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-05-05 12:25:44 +0500

    Add the watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,10 @@ module.exports = {
     filename: './public/bundle.js',
     library: 'home',
   },
+
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 100,
+    ignored: /node_modules/,
+  },
 };
