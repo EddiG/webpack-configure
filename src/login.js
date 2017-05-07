08c7635415612dd0647f8a9537e1e7ffdc8f184b
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-05-08 00:35:01 +0500

    Add lodash library as external module

--- src/login.js
+++ src/login.js
@@ -1,5 +1,11 @@
-export function login() {
+import _ from 'lodash';
+
+debugger;
+const users = [{ id: 1, name: 'Jhon' }, { id: 2, name: 'Eric' }];
+
+export function login(name) {
   alert('login was invoked from login.js');
+  console.log(_.map(users, user => user.name));
 }
 
 export default login;
--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,10 @@ module.exports = {
       },
     ],
   },
+
+  externals: {
+    lodash: '_',
+  },
 };
 
 if (isProduction) {
